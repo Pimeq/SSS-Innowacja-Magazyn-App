8910953c90fcf71ec4affbc8dca5dada7b926904
Pimeq <54513008+Pimeq@users.noreply.github.com>
2025-11-23 21:18:46 +0100

    feat: enhance app functionality with camera integration and UI improvements

--- App.tsx
+++ App.tsx
@@ -1,13 +1,48 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar"
+import { Container } from "components/Container"
+import { Text, TouchableOpacity, View } from "react-native"
+import { useColorScheme } from "nativewind"
+import { CameraView, useCameraPermissions } from "expo-camera"
+import { useState } from "react"
 
-import './global.css';
+import "./global.css"
 
 export default function App() {
-  return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
-  );
+	const { colorScheme } = useColorScheme()
+	const [lamp, setLamp] = useState(false)
+	const [permission, requestPermission] = useCameraPermissions()
+	const toggleLamp = () => {
+		setLamp(!lamp)
+	}
+
+	return (
+		<Container>
+			<StatusBar />
+
+			{permission?.granted ?
+				<Text>Perms granted</Text>
+			:	<TouchableOpacity onPress={requestPermission}>
+					<Text>No perms click here to request</Text>
+				</TouchableOpacity>
+			}
+
+			<Text>Hello world</Text>
+			<Text className="mt-4 text-blue-500">
+				{`the current color scheme is ${colorScheme}`}
+			</Text>
+
+			<View>
+				<CameraView
+					facing="back"
+					enableTorch={lamp}
+					style={{ width: 300, height: 400 }}
+				/>
+				<View>
+					<TouchableOpacity onPress={toggleLamp}>
+						<Text>toggle lamp</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</Container>
+	)
 }
