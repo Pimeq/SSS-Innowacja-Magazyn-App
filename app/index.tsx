1a88d26f06df807df061a2fc166aa634246a928a
Pimeq <54513008+Pimeq@users.noreply.github.com>
2025-11-25 19:47:50 +0100

    Refactor code structure for improved readability and maintainability

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from "expo-router"
+import { Button, Text, View } from "react-native"
+
+export default function Index() {
+	const router = useRouter()
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}>
+			<Text>Hello world</Text>
+			<Button
+				title="skanner"
+				onPress={() => {
+					router.navigate("/scanner")
+				}}
+			/>
+		</View>
+	)
+}
