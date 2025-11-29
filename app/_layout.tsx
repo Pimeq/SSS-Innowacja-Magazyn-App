17876eb4e17f4a0a79559c7a5df2527052938358
Pimeq <54513008+Pimeq@users.noreply.github.com>
2025-11-29 23:58:15 +0100

    chore: update dependencies and add nativewind support
    - Updated react-native-reanimated to version 3.17.4
    - Updated react-native-safe-area-context to version 5.4.0
    - Added install, nativewind, npm, prettier-plugin-tailwindcss, and tailwindcss dependencies
    - Updated tsconfig.json to include nativewind-env.d.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,11 @@
 import { Stack } from "expo-router"
+import "../global.css"
 
 export default function RootLayout() {
 	return (
 		<Stack
 			screenOptions={{
-				headerStyle: {
-					backgroundColor: "#f4511e",
-				},
-				headerTintColor: "#fff",
-				headerTitleStyle: {
-					fontWeight: "bold",
-				},
+				headerShown: false,
 			}}
 		/>
 	)
